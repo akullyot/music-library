b41f757b67fa1b5c483709c95eb3395e907eea77
akullyot <akullyot@gmail.com>
2023-10-09 15:53:28 -0500

    first commit

---
+++ src/Components/Gallery.js
@@ -0,0 +1,19 @@
+import GalleryItem from './GalleryItem'
+
+
+function Gallery(props){
+    //Purpose: create HTML for every item
+    const display = props.data.map((item,index) => {
+        return (
+            <GalleryItem item={item} key={index}/>
+        )
+    });
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery;
