1f6ccbdbd504e521a7416cbe8a6fdd45a91cfc9e
akullyot <akullyot@gmail.com>
2023-10-09 17:41:29 -0500

    added routing and an artist and album view

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function GalleryItem(props){
     //Purpose: determines if a user is seeing detailed view or not 
@@ -33,17 +34,27 @@ function GalleryItem(props){
             </div>
         );
     }
-    //Purpose: HTML rendered if view true, complex
+    //Purpose: HTML rendered if view true, complex with the addition of pathing links
     const detailedView = () => {
-        return(
+        return (
             <div style={detailStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
-        );
+        )
     }
+    
 
     const handleClick = () => {
         setdetailedView(!detailedview)
