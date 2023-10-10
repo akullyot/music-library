873e2b169bf23f4d547193fcc350d6edcdfb6cf7
akullyot <akullyot@gmail.com>
2023-10-09 20:57:41 -0500

    fetch album and artist data on load

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -1,15 +1,38 @@
-import { useParams } from 'react-router-dom'
-import { useState } from 'react'
-
+//Import in all required Hooks
+import { useParams}             from 'react-router-dom';
+import { useEffect, useState }  from 'react';
 
 function AlbumView() {
-    const { id } = useParams()
-    const [ albumData, setAlbumData ] = useState([])
-
+    //Purpose: pulling in variable information from the URL path variable of the apple album ID
+    const { id } = useParams();
+    //Purpose: holds all fetched, filtered album data about said artist as a state variable
+    const [ albumData, setAlbumData ] = useState([]);
+    //Purpose: fetch all information using our proxy server on every id change
+    useEffect(() => {
+        //Connecting to our proxy server backend application (to make them talk to each other, just listen where it is hosted)
+        const API_URL = `http://localhost:4000/song/${id}`; //LINK repo:RR-Improve-A-Library-Interface
+        const fetchData = async() =>
+        {
+            const response = await fetch(API_URL);
+            const resData = await response.json();
+            //Filter data to only include tracjs as the collection type
+            const tracks = resData.results.filter(item => item.wrapperType === 'track');
+            setAlbumData(tracks);
+        }
+        fetchData();
+    },[id]);
+    //Purpose: Configure album display
+    const artistDisplay = albumData.map(track => {
+        return(
+            <div key={track.trackId}>
+                    <p> {track.trackName} </p>
+            </div>
+        )
+    });
     return (
         <div>
             <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {artistDisplay}
         </div>
     )
 }
--- src/Components/ArtistView.js
+++ src/Components/ArtistView.js
@@ -1,19 +1,52 @@
 //Import in all required Hooks
-import { useParams }            from 'react-router-dom';
-import { useEffect, useState }  from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState}           from 'react';
 
 function ArtistView() {
     //Purpose: pulling in variable information from the URL path variable of the apple artist ID
     const { id } = useParams();
-    //Purpose: holds all fetched data about said artist as a state variable
+    //Purpose: holds all fetched, filtered album data about said artist as a state variable
     const [ artistData, setArtistData ] = useState([]);
-    //Purpose: fetch all information
-    //useEffect(() => {},[]);
-
+    const navigate = useNavigate();
+    //TODO move to own component
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}> Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+    //Purpose: fetch all information using our proxy server on every id change
+    useEffect(() => {
+        //Connecting to our proxy server backend application (to make them talk to each other, just listen where it is hosted)
+        const API_URL = `http://localhost:4000/album/${id}`; //LINK repo:RR-Improve-A-Library-Interface
+        const fetchData = async() =>
+        {
+            const response = await fetch(API_URL);
+            const resData = await response.json();
+            //Filter data to only include albums as the collection type
+            const albums = resData.results.filter(item => item.collectionType === 'Album');
+            setArtistData(albums);
+        }
+        fetchData();
+    },[id]);
+    //Purpose: COnfigure album display
+    const albumDisplay = artistData.map(album => {
+        return(
+            <div>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p key={album.collectionId}> {album.collectionName} </p>
+                </Link>
+            </div>
+        )
+    });
     return (
         <div>
+            {navButtons()}
             <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {albumDisplay}
         </div>
     )
 }
